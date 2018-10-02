d3afe2d8c825101302dfa99fcf87da55aece2269
KrisHolley <krisholley_09@hotmail.com>
2018-10-02 11:21:20 -0400

    pizza app

---
+++ pizza_app.rb
@@ -0,0 +1,9 @@
+def my_pizza(size, crust, cheese, toppings)
+	puts"Size: #{size}"
+	puts"Crust: #{crust}"
+	puts"Cheese: #{cheese}"
+	puts"Toppings: #{toppings}"
+	puts "______________________"
+end
+
+my_pizza("Large","Deep Dish", "American", "Pepperoni")
