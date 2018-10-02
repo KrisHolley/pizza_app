a87302bbc832b0678c69a16c4146731462645ea5
KrisHolley <krisholley_09@hotmail.com>
2018-10-02 12:54:14 -0400

    amount

--- pizza_app.rb
+++ pizza_app.rb
@@ -1,9 +1,29 @@
-def my_pizza(size, crust, cheese, toppings)
-	puts"Size: #{size}"
+def my_pizza(amount,size, crust, cheese, toppings)
+	puts"amount: #{amount}"
+	puts "size: #{size}"
 	puts"Crust: #{crust}"
 	puts"Cheese: #{cheese}"
 	puts"Toppings: #{toppings}"
 	puts "______________________"
 end
+# def amount
+# 	amount = ["1","2","4","6","8"].sample
+# end
+# def size
+# 	size = ["Medium", "Large", "Extra Large"].sample
+5.times do
+print "How many pizzas would you like?"
+amount = gets.chomp
+ 	
+my_pizza(amount,"large","Deep Dish", "American", "Pepperoni")
 
-my_pizza("Large","Deep Dish", "American", "Pepperoni")
+end
+# print "What size pizza would you like?"
+# size = gets.chomp
+# print "What style crust would you like?"
+# crust = gets.chomp
+# print "What kind of cheese would you like?"
+# cheese = gets.chomp
+# print "What toppings can we add for you?"
+# toppings = gets.chomp
+#  puts "You are ordering a #{size}, #{crust}, #{cheese}, #{toppings} pizza. Your order will be ready shortly! Thank you!"
